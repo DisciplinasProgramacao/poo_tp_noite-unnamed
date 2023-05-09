1e408cea2ec98a14b3fba5c325945d8135d83842
Vinicius <vinicius.vinibola@gmail.com>
2023-05-09 13:06:51 -0300

    criei as classes LeitorArquivo e ClienteCriador

---
+++ codigo/ClienteCriador.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+
+/*
+ * A classe ClienteCriador implementa a interface CriadorDeObjeto e define como criar um objeto do tipo Cliente a partir dos dados lidos do arquivo. Para utilizar essa classe para ler os dados dos espectadores.
+ * 
+ * 
+ * 
+ * podemos criar uma instância do LeitorArquivo no main e passar o caminho do arquivo e a instância do ClienteCriador para o método lerArquivo dessa forma:
+ * 
+ *  LeitorArquivo<Cliente> leitorArquivo = new LeitorArquivo<>();
+    List<Cliente> espectadores = leitorArquivo.lerArquivo("caminho/do/arquivo", new ClienteCriador());
+ */
+public class ClienteCriador implements LeitorArquivo.CriadorDeObjeto<Cliente> {
+
+    @Override
+    public Cliente criarObjeto(String[] dados) {
+        int id = Integer.parseInt(dados[0]);
+        String nome = dados[1];
+        String senha = dados[2];
+        String email = dados[3];
+        LinkedList<Serie> seriesFuturas = new LinkedList<>();
+        LinkedList<Serie> seriesAssistidas = new LinkedList<>();
+        Cliente cliente = new Cliente(id, nome, senha, email, seriesFuturas, seriesAssistidas);
+        return cliente;
+    }
+
+}
---
+++ codigo/LeitorArquivo.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+/*
+ * A classe LeitorArquivo recebe um parâmetro genérico T, que representa o tipo de objeto que será criado a partir do arquivo. O método lerArquivo recebe o caminho do arquivo a ser lido e um objeto do tipo CriadorDeObjeto, que é uma interface funcional que será utilizada para criar o objeto a partir dos dados lidos do arquivo.
+ * 
+ * 
+ * A interface CriadorDeObjeto também é parametrizada, recebendo o tipo do objeto a ser criado. Ela possui um único método criarObjeto que recebe um array de strings contendo os dados lidos do arquivo e retorna um objeto do tipo T. Dessa forma, é possível criar diferentes implementações dessa interface para criar objetos de diferentes classes.
+ * 
+ * 
+ * Para utilizar essa classe para ler os dados dos espectadores, podemos criar uma classe que implementa a interface CriadorDeObjeto e define como criar um objeto do tipo Cliente.
+ */
+
+public class LeitorArquivo<T> {
+    
+    /**
+     *  Método para ler arquivos passados por parametro
+     * 
+     * @param caminhoArquivo caminho do arquivo
+     * @param criadorDeObjeto objeto criado e passado por parametro
+     * @return retorna uma lista de objetos
+     * @throws FileNotFoundException
+     */
+    public List<T> lerArquivo(String caminhoArquivo, CriadorDeObjeto<T> criadorDeObjeto) throws FileNotFoundException {
+        List<T> objetos = new ArrayList<>();
+        Scanner scanner = new Scanner(new File(caminhoArquivo));
+        scanner.nextLine(); // descarta a primeira linha
+        while (scanner.hasNextLine()) {
+            String[] dados = scanner.nextLine().split(";");
+            T objeto = criadorDeObjeto.criarObjeto(dados);
+            objetos.add(objeto);
+        }
+        scanner.close();
+        return objetos;
+    }
+    
+    public interface CriadorDeObjeto<T> {
+        T criarObjeto(String[] dados);
+    }
+
+}
